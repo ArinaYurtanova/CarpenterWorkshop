64ec0b79cbbdf6eacace7bdd1c1a24e185f8e935
Арина Юртанова <Arina.yurtanova@yandex.ru>
2018-05-03 16:46:35 +0400

    Lab11

---
+++ CarpenterWorkshop/CarpenterWorkshopService/ImplementationsBD/MainServiceBD.cs
@@ -0,0 +1,167 @@
+﻿using CarpenterWorkshop;
+using CarpenterWorkshopService.BindingModels;
+using CarpenterWorkshopService.Intefaces;
+using CarpenterWorkshopService.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpenterWorkshopService.ImplementationsBD
+{
+    public class MainServiceBD : IMainService
+    {
+        private AbstractDbContext context;
+
+        public MainServiceBD(AbstractDbContext context)
+        {
+            this.context = context;
+        }
+
+        public List<OrdProductViewModel> GetList()
+        {
+            List<OrdProductViewModel> result = context.OrdProducts
+                .Select(rec => new OrdProductViewModel
+                {
+                    Id = rec.Id,
+                    CustomerID = rec.CustomerID,
+                    WoodCraftsID = rec.WoodCraftsID,
+                    WorkerID = rec.WorkerID,
+                    DateCreate = SqlFunctions.DateName("dd", rec.DateCreate) + " " +
+                                SqlFunctions.DateName("mm", rec.DateCreate) + " " +
+                                SqlFunctions.DateName("yyyy", rec.DateCreate),
+                    DateImplement = rec.DateImplement == null ? "" :
+                                        SqlFunctions.DateName("dd", rec.DateImplement.Value) + " " +
+                                        SqlFunctions.DateName("mm", rec.DateImplement.Value) + " " +
+                                        SqlFunctions.DateName("yyyy", rec.DateImplement.Value),
+                    Status = rec.Status.ToString(),
+                    Count = rec.Count,
+                    Sum = rec.Sum,
+                    CustomerFIO = rec.Customer.CustomerFIO,
+                    WoodCraftsName = rec.WoodCraft.WoodCraftsName,
+                    WorkerName = rec.Worker.WorkerFIO
+                })
+                .ToList();
+            return result;
+        }
+
+        public void CreateOrder(OrdProductBindingModel model)
+        {
+            context.OrdProducts.Add(new OrdProduct
+            {
+                CustomerID = model.CustomerID,
+                WoodCraftsID = model.WoodCraftsID,
+                DateCreate = DateTime.Now,
+                Count = model.Count,
+                Sum = model.Sum,
+                Status = ReadyProduct.Принят
+            });
+            context.SaveChanges();
+        }
+
+        public void TakeOrderInWork(OrdProductBindingModel model)
+        {
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                try
+                {
+
+                    OrdProduct element = context.OrdProducts.FirstOrDefault(rec => rec.Id == model.Id);
+                    if (element == null)
+                    {
+                        throw new Exception("Элемент не найден");
+                    }
+                    var blankCrafts = context.BlankCrafts
+                                                .Include(rec => rec.WoodBlank)
+                                                .Where(rec => rec.WoodCraftsID == element.WoodCraftsID);
+                    // списываем
+                    foreach (var blankCraft in blankCrafts)
+                    {
+                        int countOnStorages = blankCraft.Count * element.Count;
+                        var storageBlanks = context.StorageBlanks
+                                                    .Where(rec => rec.WoodBlanksID == blankCraft.WoodBlanksID);
+                        foreach (var storageBlank in storageBlanks)
+                        {
+                            // компонентов на одном слкаде может не хватать
+                            if (storageBlank.Count >= countOnStorages)
+                            {
+                                storageBlank.Count -= countOnStorages;
+                                countOnStorages = 0;
+                                context.SaveChanges();
+                                break;
+                            }
+                            else
+                            {
+                                countOnStorages -= storageBlank.Count;
+                                storageBlank.Count = 0;
+                                context.SaveChanges();
+                            }
+                        }
+                        if (countOnStorages > 0)
+                        {
+                            throw new Exception("Не достаточно компонента " +
+                                blankCraft.WoodBlank.WoodBlanksName + " требуется " +
+                                blankCraft.Count + ", не хватает " + countOnStorages);
+                        }
+                    }
+                    element.WorkerID = model.WorkerID;
+                    element.DateImplement = DateTime.Now;
+                    element.Status = ReadyProduct.Выполняется;
+                    context.SaveChanges();
+                    transaction.Commit();
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+
+        public void FinishOrder(int id)
+        {
+            OrdProduct element = context.OrdProducts.FirstOrDefault(rec => rec.Id == id);
+            if (element == null)
+            {
+                throw new Exception("Элемент не найден");
+            }
+            element.Status = ReadyProduct.Готов;
+            context.SaveChanges();
+        }
+
+        public void PayOrder(int id)
+        {
+            OrdProduct element = context.OrdProducts.FirstOrDefault(rec => rec.Id == id);
+            if (element == null)
+            {
+                throw new Exception("Элемент не найден");
+            }
+            element.Status = ReadyProduct.Оплачен;
+            context.SaveChanges();
+        }
+
+        public void PutComponentOnStock(StorageBlankBindingModel model)
+        {
+            StorageBlank element = context.StorageBlanks
+                                                .FirstOrDefault(rec => rec.StorageID == model.StorageID &&
+                                                                    rec.WoodBlanksID == model.WoodBlanksID);
+            if (element != null)
+            {
+                element.Count += model.Count;
+            }
+            else
+            {
+                context.StorageBlanks.Add(new StorageBlank
+                {
+                    StorageID = model.StorageID,
+                    WoodBlanksID = model.WoodBlanksID,
+                    Count = model.Count
+                });
+            }
+            context.SaveChanges();
+        }
+    }
+}
---
+++ CarpenterWorkshop/CarpenterWorkshopService/ImplementationsBD/WoodCraftServiceBD.cs
@@ -0,0 +1,224 @@
+﻿using CarpenterWorkshop;
+using CarpenterWorkshopService.BindingModels;
+using CarpenterWorkshopService.Intefaces;
+using CarpenterWorkshopService.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpenterWorkshopService.ImplementationsBD
+{
+    public class WoodCraftServiceBD : IWoodCraftService
+    {
+        private AbstractDbContext context;
+
+        public WoodCraftServiceBD(AbstractDbContext context)
+        {
+            this.context = context;
+        }
+
+        public List<WoodCraftViewModel> GetList()
+        {
+            List<WoodCraftViewModel> result = context.WoodCrafts
+                .Select(rec => new WoodCraftViewModel
+                {
+                    Id = rec.Id,
+                    WoodCraftsName = rec.WoodCraftsName,
+                    Price = rec.Price,
+                    BlanksCrafts = context.BlankCrafts
+                            .Where(recPC => recPC.WoodCraftsID == rec.Id)
+                            .Select(recPC => new BlankCraftViewModel
+                            {
+                                Id = recPC.Id,
+                                WoodCraftsID = recPC.WoodCraftsID,
+                                WoodBlanksID = recPC.WoodBlanksID,
+                                WoodBlanksName = recPC.WoodBlank.WoodBlanksName,
+                                Count = recPC.Count
+                            })
+                            .ToList()
+                })
+                .ToList();
+            return result;
+        }
+
+        public WoodCraftViewModel GetElement(int id)
+        {
+            WoodCraft element = context.WoodCrafts.FirstOrDefault(rec => rec.Id == id);
+            if (element != null)
+            {
+                return new WoodCraftViewModel
+                {
+                    Id = element.Id,
+                    WoodCraftsName = element.WoodCraftsName,
+                    Price = element.Price,
+                    BlanksCrafts = context.BlankCrafts
+                            .Where(recPC => recPC.WoodCraftsID == element.Id)
+                            .Select(recPC => new BlankCraftViewModel
+                            {
+                                Id = recPC.Id,
+                                WoodCraftsID = recPC.WoodCraftsID,
+                                WoodBlanksID = recPC.WoodBlanksID,
+                                WoodBlanksName = recPC.WoodBlank.WoodBlanksName,
+                                Count = recPC.Count
+                            })
+                            .ToList()
+                };
+            }
+            throw new Exception("Элемент не найден");
+        }
+
+        public void AddElement(WoodCraftBindingModel model)
+        {
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                try
+                {
+                    WoodCraft element = context.WoodCrafts.FirstOrDefault(rec => rec.WoodCraftsName == model.WoodCraftsName);
+                    if (element != null)
+                    {
+                        throw new Exception("Уже есть изделие с таким названием");
+                    }
+                    element = new WoodCraft
+                    {
+                        WoodCraftsName = model.WoodCraftsName,
+                        Price = model.Price
+                    };
+                    context.WoodCrafts.Add(element);
+                    context.SaveChanges();
+                    // убираем дубли по компонентам
+                    var groupComponents = model.BlanksCrafts
+                                                .GroupBy(rec => rec.WoodBlanksID)
+                                                .Select(rec => new
+                                                {
+                                                    WoodBlankID = rec.Key,
+                                                    Count = rec.Sum(r => r.Count)
+                                                });
+                    // добавляем компоненты
+                    foreach (var groupComponent in groupComponents)
+                    {
+                        context.BlankCrafts.Add(new BlankCraft
+                        {
+                            WoodCraftsID = element.Id,
+                            WoodBlanksID = groupComponent.WoodBlankID,
+                            Count = groupComponent.Count
+                        });
+                        context.SaveChanges();
+                    }
+                    transaction.Commit();
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+
+        public void UpdElement(WoodCraftBindingModel model)
+        {
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                try
+                {
+                    WoodCraft element = context.WoodCrafts.FirstOrDefault(rec =>
+                                        rec.WoodCraftsName == model.WoodCraftsName && rec.Id != model.Id);
+                    if (element != null)
+                    {
+                        throw new Exception("Уже есть изделие с таким названием");
+                    }
+                    element = context.WoodCrafts.FirstOrDefault(rec => rec.Id == model.Id);
+                    if (element == null)
+                    {
+                        throw new Exception("Элемент не найден");
+                    }
+                    element.WoodCraftsName = model.WoodCraftsName;
+                    element.Price = model.Price;
+                    context.SaveChanges();
+
+                    // обновляем существуюущие компоненты
+                    var compIds = model.BlanksCrafts.Select(rec => rec.WoodCraftsID).Distinct();
+                    var updateComponents = context.BlankCrafts
+                                                    .Where(rec => rec.WoodCraftsID == model.Id &&
+                                                        compIds.Contains(rec.WoodBlanksID));
+                    foreach (var updateComponent in updateComponents)
+                    {
+                        updateComponent.Count = model.BlanksCrafts
+                                                        .FirstOrDefault(rec => rec.Id == updateComponent.Id).Count;
+                    }
+                    context.SaveChanges();
+                    context.BlankCrafts.RemoveRange(
+                                        context.BlankCrafts.Where(rec => rec.WoodCraftsID == model.Id &&
+                                                                            !compIds.Contains(rec.WoodBlanksID)));
+                    context.SaveChanges();
+                    // новые записи
+                    var groupComponents = model.BlanksCrafts
+                                                .Where(rec => rec.Id == 0)
+                                                .GroupBy(rec => rec.WoodBlanksID)
+                                                .Select(rec => new
+                                                {
+                                                    WoodBlanksID = rec.Key,
+                                                    Count = rec.Sum(r => r.Count)
+                                                });
+                    foreach (var groupComponent in groupComponents)
+                    {
+                        BlankCraft elementPC = context.BlankCrafts
+                                                .FirstOrDefault(rec => rec.WoodCraftsID == model.Id &&
+                                                                rec.WoodBlanksID == groupComponent.WoodBlanksID);
+                        if (elementPC != null)
+                        {
+                            elementPC.Count += groupComponent.Count;
+                            context.SaveChanges();
+                        }
+                        else
+                        {
+                            context.BlankCrafts.Add(new BlankCraft
+                            {
+                                WoodCraftsID = model.Id,
+                                WoodBlanksID = groupComponent.WoodBlanksID,
+                                Count = groupComponent.Count
+                            });
+                            context.SaveChanges();
+                        }
+                    }
+                    transaction.Commit();
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+
+        public void DelElement(int id)
+        {
+            using (var transaction = context.Database.BeginTransaction())
+            {
+                try
+                {
+                    WoodCraft element = context.WoodCrafts.FirstOrDefault(rec => rec.Id == id);
+                    if (element != null)
+                    {
+                        // удаяем записи по компонентам при удалении изделия
+                        context.BlankCrafts.RemoveRange(
+                                            context.BlankCrafts.Where(rec => rec.WoodCraftsID == id));
+                        context.WoodCrafts.Remove(element);
+                        context.SaveChanges();
+                    }
+                    else
+                    {
+                        throw new Exception("Элемент не найден");
+                    }
+                    transaction.Commit();
+                }
+                catch (Exception)
+                {
+                    transaction.Rollback();
+                    throw;
+                }
+            }
+        }
+    }
+}
